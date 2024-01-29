c533996a59a1771cfaf5538d9f370d730e0b2753
WizzDom <41767916+manzub@users.noreply.github.com>
2024-01-29 05:55:47 +0000

    cleanup user input and bow function

--- app.py
+++ app.py
@@ -19,4 +19,20 @@ classes = pickle.load(open('labels.pkl', 'rb'))
 lemmatizer = WordNetLemmatizer()
 
 def clean_up_sentences(sentence):
-  pass
+  sentence_words = nltk.word_tokenize(sentence)
+  sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+  return sentence_words
+
+# clean up sentences and create a bag of words
+def bag_of_words(sentence, words, show_details=True):
+  sentence_words = clean_up_sentences(sentence)
+  bag = [0] * len(words)
+  for s in sentence_words:
+    # Loop through each word in the list of words
+    for i, w in enumerate(words):
+      # If word in sentence is in list of words, set corresponding position in bag to 1
+      if w == s:
+        bag[i] = 1
+        if show_details:
+          print("found in bag: %s" % w)
+  return np.array(bag)
