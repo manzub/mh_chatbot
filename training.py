7ca0f7e3ff76e154c8264dbd0606ec5b9f7be3db
WizzDom <41767916+manzub@users.noreply.github.com>
2024-01-29 05:20:37 +0000

    trained model

--- training.py
+++ training.py
@@ -87,7 +87,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accurracy'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
